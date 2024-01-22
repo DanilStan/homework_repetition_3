5affa21cb4575a3f7f27da8ea5963e6f14d346d6
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-01-22 17:55:56 +0200

    Create set localstorage function

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+let emptyObject = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  emptyObject[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(emptyObject));
+}
+
+function onLoadPage() {
+  const localValue = localStorage.getItem('feedback-form-state');
+
+  if (localValue) {
+  }
+}
