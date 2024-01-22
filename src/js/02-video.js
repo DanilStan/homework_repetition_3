f97ba786cc94bfa9b6e0485d98a028143d7dc049
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-01-22 17:18:56 +0200

    Complete task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(currentTime);
+
+function onPlay(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
