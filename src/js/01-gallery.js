149d670acd53baa025c962f15c8cf89d53504c54
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-01-22 16:24:43 +0200

    Complete task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.addEventListener('click', onClickOnImage);
+
+// Change code below this line
+const galleryMarkup = galleryItems
+  .map(image => {
+    return `<li class="gallery__item">
+<a class="gallery__link" href="${image.original}">
+<img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+</li>`;
+  })
+  .join('');
+
+galleryContainer.innerHTML = galleryMarkup;
+
+function onClickOnImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  /* options */
+});
+
+lightbox.captionPosition = 'bottom';
+lightbox.captionDelay = 250;
